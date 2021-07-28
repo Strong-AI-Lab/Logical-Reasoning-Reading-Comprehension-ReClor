0ae96884cf2f018a8d0d9733d32b01116e5a7971
Qiming Bao <741150372@qq.com>
2021-07-28 20:31:46 +1200

    Add files via upload

---
+++ scripts/run_roberta_large_MNLI_PARARULE_Plus_reclor.sh
@@ -0,0 +1,29 @@
+export RECLOR_DIR=reclor_data
+export TASK_NAME=reclor
+export MODEL_NAME=Checkpoints/pararule/roberta-large-mnli/checkpoint-11600
+export OUTPUT_NAME=roberta-large-mnli-pararule-plus-reclor
+
+CUDA_VISIBLE_DEVICES=0 python run_multiple_choice.py \
+    --model_type roberta \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --evaluate_during_training \
+    --do_test \
+    --do_lower_case \
+    --data_dir $RECLOR_DIR \
+    --max_seq_length 256 \
+    --per_gpu_eval_batch_size 1   \
+    --per_gpu_train_batch_size 1   \
+    --gradient_accumulation_steps 24 \
+    --learning_rate 1e-05 \
+    --num_train_epochs 10.0 \
+    --output_dir Checkpoints/$TASK_NAME/${OUTPUT_NAME} \
+    --fp16 \
+    --logging_steps 200 \
+    --save_steps 200 \
+    --adam_betas "(0.9, 0.98)" \
+    --adam_epsilon 1e-6 \
+    --no_clip_grad_norm \
+    --warmup_proportion 0.1 \
+    --weight_decay 0.01
